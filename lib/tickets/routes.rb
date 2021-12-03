4f9209fd9ba053e7fe98255371cff4d93c108c15
nickchomey <88559987+nickchomey@users.noreply.github.com>
2021-12-03 14:46:15 -0600

    .
    Signed-off-by: nickchomey <88559987+nickchomey@users.noreply.github.com>

--- lib/tickets/routes.rb
+++ lib/tickets/routes.rb
@@ -3,9 +3,9 @@ Tickets::Engine.routes.draw do
 end
 
 Discourse::Application.routes.append do
-  =begin get '/admin/tickets' => 'admin/plugins#index', constraints: StaffConstraint.new
-  mount Tickets::Engine, at: '/', constraints: StaffConstraint.new =end
+  get '/admin/tickets' => 'admin/plugins#index', constraints: StaffConstraint.new
+  mount Tickets::Engine, at: '/', constraints: StaffConstraint.new
   
-  get '/admin/tickets' => 'admin/plugins#index'
-  mount Tickets::Engine, at: '/'
+ =begin  get '/admin/tickets' => 'admin/plugins#index'
+  mount Tickets::Engine, at: '/' =end
 end
